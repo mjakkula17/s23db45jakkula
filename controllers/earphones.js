248349e3020f6b8e161a7eae95c44cf34690c151
Jakkula <S560445@nwmissouri.edu>
2023-04-18 21:14:06 -0500

    changes made

--- controllers/earphones.js
+++ controllers/earphones.js
@@ -102,3 +102,31 @@ ${JSON.stringify(req.body)}`)
 failed`);
  }
 };
+
+// Handle a show one view with id specified by query
+exports.earphones_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await earphones.findById( req.query.id)
+    res.render('earphonesdetail',
+   { title: 'earphones Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   
+   // Handle building the view for creating a earphones.
+   // No body, no in path parameter, no query.
+   // Does not need to be async
+exports.earphones_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('earphonescreate', { title: 'earphones Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/earphones.js
+++ routes/earphones.js
@@ -10,5 +10,9 @@ var router = express.Router();
 
 /* GET earphoness */
 router.get('/', earphones_controlers.earphones_view_all_Page );
+/* GET detail earphones page */
+router.get('/detail', earphones_controlers.earphones_view_one_Page);
+/* GET create earphones page */
+router.get('/create', earphones_controlers.earphones_create_Page);
 module.exports = router;
 
