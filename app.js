09900021576ffcc709fcb7abe4d7588559b6ed4f
Jakkula <S560445@nwmissouri.edu>
2023-04-21 10:41:51 -0500

    made changes

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }
+  ));
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -25,6 +42,7 @@ var selectorRouter = require('./routes/selector');
 var earphones = require("./models/earphones");
 var resourceRouter = require('./routes/resource');
 
+
 var app = express();
 
 // view engine setup
@@ -35,6 +53,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
