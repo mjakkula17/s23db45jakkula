e96112f95c76fe56327fe8280ce34279bc9d9b86
Jakkula <S560445@nwmissouri.edu>
2023-04-25 01:27:17 -0500

    changes made

--- models/earphones.js
+++ models/earphones.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const earphonesSchema = mongoose.Schema({
-earphones_color: String,
-earphones_length: String,
-earphones_cost: String,
-earphones_sound: String
+    earphones_color: {type: String, minlength: 1, maxlength: 50},
+    earphones_length: {type: String, minlength: 1, maxlength: 50},
+    earphones_cost: {type: String, minlength: 10, maxlength: 1550},
+    earphones_sound: {type: String, minlength: 1, maxlength: 50}
 })
 module.exports = mongoose.model("earphones", earphonesSchema)
--- routes/earphones.js
+++ routes/earphones.js
@@ -7,6 +7,18 @@ var router = express.Router();
   res.render('earphones', { title: 'Search Results earphones' });
 }); */
 
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+
+/* GET update earphones page */
+router.get('/update', secured, earphones_controlers.earphones_update_Page);
 
 /* GET earphoness */
 router.get('/', earphones_controlers.earphones_view_all_Page );
@@ -14,8 +26,8 @@ router.get('/', earphones_controlers.earphones_view_all_Page );
 router.get('/detail', earphones_controlers.earphones_view_one_Page);
 /* GET create earphones page */
 router.get('/create', earphones_controlers.earphones_create_Page);
-/* GET create update page */
-router.get('/update', earphones_controlers.earphones_update_Page);
+/* GET create update page 
+router.get('/update', earphones_controlers.earphones_update_Page);*/
 /* GET delete earphones page */
 router.get('/delete', earphones_controlers.earphones_delete_Page);
 module.exports = router;
