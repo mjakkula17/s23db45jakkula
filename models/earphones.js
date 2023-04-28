ea0e64527bdf1a13dfe528073cd968a83609dc53
Jakkula <S560445@nwmissouri.edu>
2023-04-27 22:14:50 -0500

    Update earphones.js

--- models/earphones.js
+++ models/earphones.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const earphonesSchema = mongoose.Schema({
-    earphones_color: {type: String, minlength: 1, maxlength: 50},
-    earphones_length: {type: String, minlength: 1, maxlength: 50},
-    earphones_cost: {type: String, minlength: 10, maxlength: 1550},
-    earphones_sound: {type: String, minlength: 1, maxlength: 50}
+    earphones_color: {type: String, min: 1, max: 50},
+    earphones_length: {type: String, min: 1, maxl: 50},
+    earphones_cost: {type: String, min: 1, max: 1550},
+    earphones_sound: {type: String, min: 1, max: 50}
 })
 module.exports = mongoose.model("earphones", earphonesSchema)
